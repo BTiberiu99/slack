3ffdf8f3f0374b8cf9fa458054b334e503aade53
Tiberiu Baron <barontiberiu@gmail.com>
2020-02-09 18:17:52 +0200

    update

--- stats.go
+++ stats.go
@@ -12,6 +12,10 @@ import (
 	"github.com/ztrue/tracerr"
 )
 
+const (
+	GB = float64(1024 * 1024 * 1024)
+)
+
 type Stats struct {
 	thresholdMemory float64
 	thresholdCPU    float64
@@ -81,6 +85,7 @@ func (s *Stats) sendStats() error {
 	}
 
 	before, err := cpu.Get()
+
 	if err != nil {
 		return tracerr.Wrap(err)
 	}
@@ -94,25 +99,21 @@ func (s *Stats) sendStats() error {
 
 	total := float64(after.Total - before.Total)
 
-	div := float64(1024 * 1024 * 1024)
-
-	markRedMem := ""
+	markRedMem, markRedCpu := "", ""
 
 	if float64(memory.Total-memory.Used)/(1024*1024) < s.thresholdMemory {
 		markRedMem = Red
 	}
-
-	markRedCpu := ""
 
 	if float64(after.User-before.User)/total*100 > s.thresholdCPU {
 		markRedCpu = Red
 	}
 
 	err = s.report.Stats(s.appName,
-		fmt.Sprintf("Memory Total: %0.3f GB\n", float64(memory.Total)/div),
-		fmt.Sprintf("%sMemory Used: %0.3f GB\n", markRedMem, float64(memory.Used)/div),
-		fmt.Sprintf("Memory Cached:  %0.3f GB\n", float64(memory.Cached)/div),
-		fmt.Sprintf("Memory Free:  %0.3f GB\n", float64(memory.Free)/div),
+		fmt.Sprintf("Memory Total: %0.3f GB\n", float64(memory.Total)/GB),
+		fmt.Sprintf("%sMemory Used: %0.3f GB\n", markRedMem, float64(memory.Used)/GB),
+		fmt.Sprintf("Memory Cached:  %0.3f GB\n", float64(memory.Cached)/GB),
+		fmt.Sprintf("Memory Free:  %0.3f GB\n", float64(memory.Free)/GB),
 		fmt.Sprintf("%sCPU user: %0.2f %% \n", markRedCpu, float64(after.User-before.User)/total*100),
 		fmt.Sprintf("CPU system: %0.2f %%\n", float64(after.System-before.System)/total*100),
 		fmt.Sprintf("CPU idle: %0.2f %%\n", float64(after.Idle-before.Idle)/total*100),
